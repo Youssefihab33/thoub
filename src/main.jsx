a2d1ce0904da0695919becc6671fde42754b2682
Youssef Ihab <youssefihab33@gmail.com>
2025-02-16 20:47:21 +0200

    switched to normal bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import './assets/bootstrap.5.3.3.min.js';
 
 import Header from './components/Header';
 import Content from './components/Content';
